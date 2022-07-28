f676010513bddff44c03d37739e210edb7d5a48a
omarmoha15 <lordomar15@gmail.com>
2022-07-28 19:03:03 +0300

    ReactApp

--- src/componants/Home.js
+++ src/componants/Home.js
@@ -1,3 +1,4 @@
+import { Switch } from 'antd';
 import React, { useState } from 'react';
 import Users from '../MOCK_DATA.json';
 import Table from './Table';
@@ -6,6 +7,32 @@ import Table from './Table';
 function Home() {
 
     const [query,setQuery] =useState("");
+    const[order,setOrder] = useState("ASC");
+    const[dataa,setDataa]= useState(Users);
+    const[toggle,setToggle] = useState("false");
+
+    const sorting = (col) =>{
+      if(order === "ASC"){
+      const sorted = [...dataa].sort((a,b) =>
+      a[col].toLowerCase() > b[col].toLowerCase() ? 1:-1
+      );
+        setDataa(sorted);
+    setOrder("DSC");
+    }
+    if(order === "DSC"){
+      const sorted = [...dataa].sort((a,b) =>
+      a[col].toLowerCase() < b[col].toLowerCase() ? 1:-1
+      );
+        setDataa(sorted);
+    setOrder("ASC");
+    }
+    }
+
+        const toggler=()=>{
+         toggle ? setToggle(false) : setToggle(true);
+
+        }
+
     const search =(data) => {
         return data.filter((item) => item.hero_name.toLowerCase().includes(query));
 
@@ -18,6 +45,11 @@ function Home() {
        className='search'
        onChange={e=> setQuery(e.target.value)}
        />
+       <p>sorting by power:</p>
+
+        <Switch className='switch'
+          onClick={toggler}/>
+         {toggle ? <span>{()=>sorting(dataa.hero_name)}</span> : <span>{()=>sorting(dataa.power)}</span> }
 
        <Table data={search(Users)}/>
 
--- src/componants/Table.jsx
+++ src/componants/Table.jsx
@@ -1,4 +1,4 @@
-import { Switch } from 'antd';
+
 import React, { useState } from 'react'
 import ReactStars from 'react-rating-stars-component';
 import { Link } from 'react-router-dom';
@@ -6,45 +6,16 @@ import { Link } from 'react-router-dom';
 
 
 function Table({data}) {
-  const[dataa,setDataa]= useState({data});
-  const[order,setOrder] = useState("ASC");
+
+
     const [rating,setRating] = useState(null);
-    const[toggle,setToggle] = useState("false");
 
-const sorting = (col) =>{
-  if(order === "ASC"){
-  const sorted = [...data].sort((a,b) =>
-  a[col].toLowerCase() > b[col].toLowerCase() ? 1:-1
-  );
-    setDataa(sorted);
-setOrder("DSC");
-}
-if(order === "DSC"){
-  const sorted = [...data].sort((a,b) =>
-  a[col].toLowerCase() < b[col].toLowerCase() ? 1:-1
-  );
-    setDataa(sorted);
-setOrder("ASC");
-}
-}
-
-    const toggler=()=>{
-     toggle ? setToggle(false) : setToggle(true);
-
-    }
 
   return (
 
     <table className="table">
-      <caption>
-        sorting by power:
-         <Switch className='switch'
-          onClick={toggler}/>
-          {toggle ? <span>{()=>sorting(data.hero_name)}</span> : <span>{()=>sorting(data.power)}</span> }
-        </caption>
 
-
-    <tbody>
+        <tbody>
                 <tr >
                     <th>Hero Name</th>
                     <th>Power</th>
@@ -58,8 +29,8 @@ setOrder("ASC");
                     <td>{<ReactStars/>}</td>
                 </tr>
                 ))}
-            </tbody>
-            </table>
+         </tbody>
+    </table>
   )
 }
 
